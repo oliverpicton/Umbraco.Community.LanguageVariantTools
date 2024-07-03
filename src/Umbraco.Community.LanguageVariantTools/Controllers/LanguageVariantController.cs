481689412f841dfca4d69782237b7ef390f9672a
Oliver Picton <oliver@enjoy-digital.co.uk>
2024-07-03 11:00:52 +0100

    Bug fix - now removes actual property values when removing language variant.

--- src/Umbraco.Community.LanguageVariantTools/Controllers/LanguageVariantController.cs
+++ src/Umbraco.Community.LanguageVariantTools/Controllers/LanguageVariantController.cs
@@ -1,9 +1,12 @@
-﻿using Umbraco.Cms.Core.Models;
+﻿
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Web.BackOffice.Controllers;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Community.LanguageVariantTools.Models;
 using Umbraco.Extensions;
+using System.Linq;
 
 namespace Umbraco.Community.LanguageVariantTools.Controllers
 {
@@ -12,129 +15,148 @@ namespace Umbraco.Community.LanguageVariantTools.Controllers
     {
         private readonly IContentService _contentService;
         private readonly ILocalizationService _localizationService;
+        private readonly ILogger<LanguageVariantController> _logger;
 
-        public LanguageVariantController(IContentService contentService, ILocalizationService localizationService)
+        public LanguageVariantController(IContentService contentService, ILocalizationService localizationService, ILogger<LanguageVariantController> logger)
         {
             _contentService = contentService;
             _localizationService = localizationService;
+            _logger = logger;
         }
 
         public VariantResult Remove(int nodeId, string culture)
         {
-            try
+            var content = _contentService.GetById(nodeId);
+
+            var response =  new VariantResult
             {
-                var node = _contentService.GetById(nodeId);
+                IsSuccess = false,
+            };
 
-                node.CultureInfos.Remove(culture.ToLower());
-
-                var result = _contentService.Save(node);
-
-                return new VariantResult
-                {
-                    IsSuccess = true,
-                };
-
+            if (content == null)
+            {
+                var message = "No content node found with id {Id}.";
+                _logger.LogCritical(message, nodeId);
+                response.AddError(message, nodeId);
+                return response;
             }
-            catch (Exception e)
+
+            if (string.IsNullOrEmpty(culture))
             {
-                return new VariantResult
+                var message = "Provided culture is null or empty.";
+                _logger.LogCritical(message);
+                response.AddError(message);
+                return response;
+            }
+
+            string cultureLower = culture.ToLower();
+
+            if (content.CultureInfos != null && content.IsCultureAvailable(culture))
+            {
+                content.CultureInfos.Remove(cultureLower);
+
+                foreach (var property in content.Properties)
                 {
-                    IsSuccess = false,
-                    Error = new Error
+                    var valuesToRemove = property.Values
+                        .Where(value => value.Culture?.Equals(culture, StringComparison.OrdinalIgnoreCase) == true)
+                        .ToList();
+
+                    foreach (var value in valuesToRemove)
                     {
-                        Message = e.Message,
-                    },
-                };
+                        property.SetValue(null, value.Culture, value.Segment);
+                    }
+                }
+
+                var result = _contentService.Save(content);
+
+                if (result.Success)
+                {
+                    response.IsSuccess = true;
+                }
+                else
+                {
+                    var message = "Failed to save content";
+                    _logger.LogCritical(message, culture);
+
+                    response.AddError(message, culture);
+                }
             }
+            else
+            {
+                var message = "Culture {culture} not found in the collection.";
+                _logger.LogCritical(message, culture);
+
+                response.AddError(message, culture);
+            }
+
+            return response;
         }
     
         public VariantResult Create(int? nodeId, bool includeChildren, string culture)
         {
-            try
+            var response = new VariantResult
             {
-                if (nodeId is null)
+                IsSuccess = false
+            };
+
+            if (nodeId is null)
+            {
+                response.AddError("NodeId is not set");
+                return response;
+            }
+
+            var contentItems = new List<IContent>();
+            var contentItem = _contentService.GetById(nodeId.Value);
+
+            if (contentItem == null)
+            {                  
+                response.AddError("content is not found");
+                return response;
+            }
+
+            contentItems.Add(contentItem);
+
+            if (includeChildren)
+            {
+                contentItems.AddRange(_contentService.GetPagedChildren(contentItem.Id, 0, int.MaxValue, out long totalRecords));
+            }
+
+            var languages = _localizationService.GetAllLanguages();
+
+            foreach (var content in contentItems)
+            {
+                var contentItemLanguages = content.AvailableCultures;
+
+                foreach (var targetCulture in languages.Select(x => x.CultureInfo.Name))
                 {
-                    return new VariantResult
+                    var isCreated = contentItemLanguages.Any(x => x == targetCulture.ToLower());
+
+                    if (isCreated) continue;
+
+                    content.SetCultureName(content.Name, targetCulture);
+
+                    foreach (var property in content.Properties)
                     {
-                        IsSuccess = false,
-                        Error = new Error
+                        var value = property.Values.FirstOrDefault(p => p.Culture != null && p.Culture.ToLower() == culture.ToLower())?.EditedValue;
+
+                        if (value is not null)
                         {
-                            Message = "NodeId is not set",
-                        },
-                    };
-                }
-
-                var contentItems = new List<IContent>();
-                var contentItem = _contentService.GetById(nodeId.Value);
-
-                if (contentItem == null)
-                {
-                    return new VariantResult
-                    {
-                        IsSuccess = false,
-                        Error = new Error
-                        {
-                            Message = "content is not found",
-                        },
-                    };
-                }
-
-                contentItems.Add(contentItem);
-
-                if (includeChildren)
-                {
-                    contentItems.AddRange(_contentService.GetPagedChildren(contentItem.Id, 0, int.MaxValue, out long totalRecords));
-                }
-
-                var languages = _localizationService.GetAllLanguages();
-
-                foreach (var content in contentItems)
-                {
-                    var contentItemLanguages = content.AvailableCultures;
-
-                    foreach (var targetCulture in languages.Select(x => x.CultureInfo.Name))
-                    {
-                        var isCreated = contentItemLanguages.Any(x => x == targetCulture.ToLower());
-
-                        if (isCreated) continue;
-
-                        content.SetCultureName(content.Name, targetCulture);
-
-                        foreach (var property in content.Properties)
-                        {
-                            var value = property.Values.FirstOrDefault(p => p.Culture != null && p.Culture.ToLower() == culture.ToLower())?.EditedValue;
-
-                            if (value is not null)
+                            try
                             {
-                                try
-                                {
-                                    content.SetValue(property.Alias, value, targetCulture);
-                                }
-                                catch { continue; }
+                                content.SetValue(property.Alias, value, targetCulture);
                             }
+                            catch { continue; }
                         }
                     }
-
-                    _contentService.Save(content);
                 }
 
-                return new VariantResult
-                {
-                    IsSuccess = true,
-                };
+                _contentService.Save(content);
+            }
 
-            }
-            catch (Exception e)
+            return new VariantResult
             {
-                return new VariantResult
-                {
-                    IsSuccess = false,
-                    Error = new Error
-                    {
-                        Message = e.Message,
-                    },
-                };
-            }
+                IsSuccess = true,
+            };          
         }
     }
 }
--- src/Umbraco.Community.LanguageVariantTools/Models/VariantResult.cs
+++ src/Umbraco.Community.LanguageVariantTools/Models/VariantResult.cs
@@ -1,9 +1,25 @@
-﻿namespace Umbraco.Community.LanguageVariantTools.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Umbraco.Community.LanguageVariantTools.Models
 {
     public class VariantResult
     {
         public bool IsSuccess { get; set; }
 
-        public Error? Error { get; set; }
+        public Error? Error { get; private set; }
+
+        public void AddError(string message, params object?[] arg)
+        {
+            if (arg != null && arg.Length > 0)
+            {
+                message = Regex.Replace(message, @"\{(\d+)\}", match =>
+                {
+                    int index = int.Parse(match.Groups[1].Value);
+                    return index < arg.Length ? arg[index]?.ToString() ?? string.Empty : match.Value;
+                });
+            }
+
+            Error = new Error { Message = message };
+        }
     }
 }
